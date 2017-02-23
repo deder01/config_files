e6c85a83d0e6049d877f772d4f82a7886d388ade
deder <david.eder26@gmail.com>
2017-02-23 16:28:46 -0500

    Remmaped accept key to shift-tab

--- .zsh_custom/zsh-autosuggestions-config.zsh
+++ .zsh_custom/zsh-autosuggestions-config.zsh
@@ -1,1 +1,5 @@
+# Make the suggestions show up in white colored text so they are readable
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=white'
+
+# Bind shift-tab to accept an autosuggest
+bindkey '^[[Z' autosuggest-accept
