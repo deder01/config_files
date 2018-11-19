888a5953fa0d224df9dd79a45f2eb71079944eb8
David Eder <david.eder26@gmail.com>
2018-11-19 12:48:50 -0600

    Correct fzf path for work mac

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/deder/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/deder/.fzf/bin"
+if [[ ! "$PATH" == */Users/eder/.fzf/bin* ]]; then
+  export PATH="$PATH:/Users/eder/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/deder/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/eder/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/fzf/key-bindings.zsh"
+source "/Users/eder/.fzf/shell/key-bindings.zsh"
 
