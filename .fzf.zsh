6b2fea819661601e4198f8f275da1d3c00a47f16
David Eder <eder@amazon.com>
2024-12-24 11:31:23 -0500

    Updates to path

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,7 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/deder/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/deder/.fzf/bin"
+  PATH="${PATH:+${PATH}:}/home/deder/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/home/deder/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/home/deder/.fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
